eadbe8412b127ca09aa763e5706bbf4756f548f7
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-03-29 20:18:49 +0530

    abi and address added to constants

--- Frontend/Constants/Constants.js
+++ Frontend/Constants/Constants.js
@@ -1,2 +1,68 @@
-export const WHITELIST_CONTRACT_ADDRESS = "YOUR_WHITELIST_CONTRACT_ADDRESS";
-export const abi = YOUR_ABI;
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0xd3Bf6e15a6da6392892187a83c6c19bcc35b0375";
+
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "_maxWhitelistedAddresses",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxWhitelistedAddresses",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAddressesWhitelisted",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelistedAddresses",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
